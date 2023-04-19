b5cdf8ab52778ec94b26abc2694290654487ebaf
Kahf Hussain <kahfnhussain@gmail.com>
2023-04-19 13:30:40 +0200

    Creating UI for login screen

--- note_app/lib/views/homeScreen.dart
+++ note_app/lib/views/homeScreen.dart
@@ -1,17 +1,90 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
+  State<LoginScreen> createState() => _LoginScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Login Screen"),
+        // actions: [
+        //   Icon(Icons.more_vert),
+        // ],
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          child: Column(
+            children: [
+              Container(
+                alignment: Alignment.center,
+                height: 300.0,
+                child: Lottie.asset("assets/38435-register.json"),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 30.0),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.email),
+                    hintText: 'Email',
+                    enabledBorder: OutlineInputBorder(),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(horizontal: 30.0),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.password),
+                    hintText: 'Password',
+                    enabledBorder: OutlineInputBorder(),
+                    suffixIcon: Icon(Icons.visibility),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text("Login"),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Container(
+                child: Card(
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Text("Forgot Password"),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 2.0,
+              ),
+              Container(
+                child: Card(
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Text("Don't have an accont: SignUp"),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
